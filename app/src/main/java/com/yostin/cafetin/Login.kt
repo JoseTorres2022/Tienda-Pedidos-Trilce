3f80f4630a1bd2aad93c6fa40ea0605d4aeda1a5
JustinNajarro <jnajarroVillagaray@gmail.com>
2024-02-18 18:41:54 -0500

    Login-Firebase

--- app/src/main/java/com/yostin/cafetin/Login.kt
+++ app/src/main/java/com/yostin/cafetin/Login.kt
@@ -2,10 +2,45 @@ package com.yostin.cafetin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Login : AppCompatActivity() {
+
+    private lateinit var txtEmail: EditText
+    private lateinit var txtPassword: EditText
+    private lateinit var btnLogin: Button
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        txtEmail = findViewById(R.id.txtEmail)
+        txtPassword = findViewById(R.id.txtPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+
+        btnLogin.setOnClickListener {
+            val email = txtEmail.text.toString()
+            val password = txtPassword.text.toString()
+
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("MainActivity", "Inicio de sesión exitoso")
+                        val user: FirebaseUser? = firebaseAuth.currentUser
+                    } else {
+                        Log.w("MainActivity", "Error en el inicio de sesión", task.exception)
+                    }
+                }
+        }
+
     }
 }
