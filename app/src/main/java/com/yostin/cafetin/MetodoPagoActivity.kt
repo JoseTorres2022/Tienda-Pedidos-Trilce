ef19ffc51b715b7c822f579e127406550e149001
goten <egobadentoxis@gmail.com>
2024-03-02 21:26:30 -0500

    metodo pago y detalle producto

---
+++ app/src/main/java/com/yostin/cafetin/MetodoPagoActivity.kt
@@ -0,0 +1,11 @@
+package com.yostin.cafetin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MetodoPagoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_metodo_pago)
+    }
+}
